6cdb091ae4d6a32abfe193d125f87e25f467c789
Gleb Solovev <glebsolo46@gmail.com>
2024-06-25 18:26:01 +0300

    add chages for ice case

--- functions.py
+++ functions.py
@@ -3,8 +3,9 @@ import torch
 from torch.cuda import amp
 from torch.cuda.amp import autocast as autocast
 from utils import compute_metrics, visualize
-
+from torch.utils.tensorboard import SummaryWriter
 scaler = amp.GradScaler()
+writer = SummaryWriter(f'writer/lstm')
 
 
 def model_forward_single_layer(model, inputs, targets_len, num_layers):
@@ -54,9 +55,11 @@ def train(args, logger, epoch, model, train_loader, criterion, optimizer):
     num_batches = len(train_loader)
     losses = []
 
-    for batch_idx, (inputs, targets) in enumerate(train_loader):
-
+    for batch_idx, batch in enumerate(train_loader):
+        inputs, targets,x_d,y_d = batch
         optimizer.zero_grad()
+        inputs = inputs.permute(0, 2, 1, 3, 4)
+        targets = targets.permute(0, 2, 1, 3, 4)
 
         inputs, targets = map(lambda x: x.float().to(args.device), [inputs, targets])
         targets_len = targets.shape[1]
@@ -74,7 +77,7 @@ def train(args, logger, epoch, model, train_loader, criterion, optimizer):
         scaler.scale(loss).backward()
         scaler.step(optimizer)
         scaler.update()
-
+        writer.add_scalar('Loss_train',loss.item(),(epoch*train_loader.__len__())+batch_idx)
         losses.append(loss.item())
 
         if batch_idx and batch_idx % args.log_train == 0:
@@ -88,12 +91,14 @@ def test(args, logger, epoch, model, test_loader, criterion, cache_dir):
     num_batches = len(test_loader)
     losses, mses, ssims = [], [], []
 
-    for batch_idx, (inputs, targets) in enumerate(test_loader):
-
+    for batch_idx, batch in enumerate(test_loader):
+        inputs, targets,x_d,y_d = batch
         with torch.no_grad():
             inputs, targets = map(lambda x: x.float().to(args.device), [inputs, targets])
+            
+            inputs = inputs.permute(0, 2, 1, 3, 4)
+            targets = targets.permute(0, 2, 1, 3, 4)
             targets_len = targets.shape[1]
-
             if args.model == 'SwinLSTM-B':
                 outputs = model_forward_single_layer(model, inputs, targets_len, args.depths)
 
@@ -102,17 +107,19 @@ def test(args, logger, epoch, model, test_loader, criterion, cache_dir):
 
             outputs = torch.stack(outputs).permute(1, 0, 2, 3, 4).contiguous()
             targets_ = torch.cat((inputs[:, 1:], targets), dim=1)
-
+            
             losses.append(criterion(outputs, targets_).item())
-
+            writer.add_scalar('Loss_tesst',losses[-1],(epoch*test_loader.__len__())+batch_idx)
             inputs_len = inputs.shape[1]
             outputs = outputs[:, inputs_len - 1:]
 
             mse, ssim = compute_metrics(outputs, targets)
-
+            writer.add_images('Ground_truth', np.expand_dims(targets.squeeze(2).detach().cpu().numpy()[0],axis=1), 0)
+            writer.add_images('Predicts', np.expand_dims(outputs.squeeze(2).detach().cpu().numpy()[0],axis=1), 0)
             mses.append(mse)
             ssims.append(ssim)
-
+            writer.add_scalar('mses',mse,(epoch*test_loader.__len__())+batch_idx)
+            writer.add_scalar('ssims',ssim,(epoch*test_loader.__len__())+batch_idx)
             if batch_idx and batch_idx % args.log_valid == 0:
                 logger.info(
                     f'EP:{epoch:04d} BI:{batch_idx:03d}/{num_batches:03d} Loss:{np.mean(losses):.6f} MSE:{mse:.4f} SSIM:{ssim:.4f}')
--- train.py
+++ train.py
@@ -4,38 +4,155 @@ from configs import get_args
 from functions import train, test
 from torch.utils.data import DataLoader
 from dataset import Moving_MNIST
+from read_npy import create_dataloaders
+from skimage.transform import resize
+
+def check_size_for_reize(img_size,num_heads):
+    if img_size[0]%num_heads!=0:
+        img_size_new = [(int(img_size[0]/num_heads)+1)*num_heads,img_size[1]]
+        print("New image size is",img_size_new)
+        return img_size_new
+    else:
+        return img_size
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+def embed_dim_by_img(img,num_heads,emb_mult):
+    emb_dim = img*emb_mult
+    head_det = emb_dim%num_heads
+    if head_det!=0:
+        emb_dim=emb_dim-head_det+num_heads
+    return emb_dim
+def count_patch_size(imgsize):
+    patch = imgsize**0.5
+    if imgsize%patch==0:
+        return patch
+    else:
+        while imgsize%patch!=0:
+            patch = int(patch)-1
+    return patch
+
+
+
+
+#Loss f-n
+loss_l1 = torch.nn.L1Loss()
+
+
+
 
 
 def setup(args):
-    if args.model == 'SwinLSTM-B':
-        from SwinLSTM_B import SwinLSTM
-        model = SwinLSTM(img_size=args.input_img_size, patch_size=args.patch_size,
-                         in_chans=args.input_channels, embed_dim=args.embed_dim,
-                         depths=args.depths, num_heads=args.heads_number,
-                         window_size=args.window_size, drop_rate=args.drop_rate,
-                         attn_drop_rate=args.attn_drop_rate, drop_path_rate=args.drop_path_rate).to(args.device)
+    
 
+    # train_dataset = Moving_MNIST(args, split='train')
+    # valid_dataset = Moving_MNIST(args, split='valid')
+    path_to_mask = r'D:\Projects\test_cond\AAAI_code\Ice\coastline_masks'
+    path_to_sea = r'D:\Projects\test_cond\AAAI_code\Ice'
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    accumulation_steps = 8
+    lr_max = 0.0005
+    lr_min = 0.00001
+    epochs = 90
+    predict_period = 26
+    in_period = 52
+    batch_size = 4
+    num_heads=8
+    emb_mult=4
+    place='kara'
+    from_ymd_train=[1979, 1, 1]
+    to_ymd_train=[2012,1,1]
+    from_ymd_test=[2012,1,2]
+    to_ymd_test=[2020, 1, 1]
+    stride = 7
+    mask = np.load(fr'{path_to_mask}\{place}_mask.npy')
+    resize_img = [64, 64]
+    if resize_img is not None:
+        resize_img = check_size_for_reize(resize_img,num_heads=num_heads)
+        mask = np.load(fr'{path_to_mask}\{place}_mask.npy')
+        mask = resize(mask, (resize_img[0], resize_img[1]), anti_aliasing=False)
+    dataloader_train, img_sizes = create_dataloaders(path_to_dir=f'{path_to_sea}/{place}',
+                                            batch_size=batch_size,
+                                            in_period=in_period,
+                                            predict_period=predict_period,
+                                            stride=stride,
+                                            test_end=None,
+                                            from_ymd=from_ymd_train,
+                                            to_ymd=to_ymd_train,
+                                            pad=False,
+                                            train_test_split=None,
+                                             resize_img=resize_img)
+    dataloader_test, img_sizes = create_dataloaders(path_to_dir=f'{path_to_sea}/{place}',
+                                                batch_size=1,
+                                                in_period=in_period,
+                                                predict_period=predict_period,
+                                                stride=stride,
+                                                test_end=None,
+                                                from_ymd=from_ymd_test,
+                                                to_ymd=to_ymd_test,
+                                                pad=False,
+                                                train_test_split=None,
+                                                resize_img=resize_img)
+    # dataloaders, img_sizes = create_dataloaders(path_to_dir=f'{path_to_sea}/{place}',
+    #                                             batch_size=batch_size,
+    #                                             in_period=in_period,
+    #                                             predict_period=predict_period,
+    #                                             stride=stride,
+    #                                             test_end=None,
+    #                                             from_ymd=from_ymd,
+    #                                             to_ymd=to_ymd,
+    #                                             pad=False,
+    #                                             resize_img=resize_img)
+    train_len = dataloader_train.__len__()
+    test_len = dataloader_test.__len__()
+    
+    if img_sizes[1]>img_sizes[0]:
+        img_sizes=(img_sizes[1],img_sizes[0])
+    if img_sizes[1]!=img_sizes[0]:
+        patch_size1 = count_patch_size(img_sizes[0]) #int(img_sizes[0]/(img_sizes[0]*2)**0.5)
+        patch_size2 = count_patch_size(img_sizes[1])
+        patch_size=[patch_size1,patch_size2]#int(img_sizes[1]/(img_sizes[1]*2)**0.5)
+    else:
+        patch_size = int(img_sizes[0]/(img_sizes[0]*2)**0.5)
+    #shape_end = (img_sizes[0]//patch_size)**2*in_period
+    embed_dim = 128#embed_dim_by_img(img_sizes[1],num_heads,emb_mult)
+    # args.window_size = 6
+    # args.input_size = img_sizes
+    # args.image_size = img_sizes
+    # args.train_batch_size = batch_size
+    # args.patch_size = patch_size
+    #args.model = 'SwinLSTM-B' 
+
+    # if args.model == 'SwinLSTM-B':
+    #     from SwinLSTM_B import SwinLSTM
+    #     model = SwinLSTM(img_size=img_sizes, patch_size=patch_size,
+    #                      in_chans=in_period, embed_dim=embed_dim,
+    #                      depths=args.depths, num_heads=args.heads_number,
+    #                      window_size=args.window_size, drop_rate=args.drop_rate,
+    #                      attn_drop_rate=args.attn_drop_rate, drop_path_rate=args.drop_path_rate).to(args.device)
+    
     if args.model == 'SwinLSTM-D':
         from SwinLSTM_D import SwinLSTM
         model = SwinLSTM(img_size=args.input_img_size, patch_size=args.patch_size,
-                         in_chans=args.input_channels, embed_dim=args.embed_dim,
+                         in_chans=args.input_channels, embed_dim=embed_dim,
                          depths_downsample=args.depths_down, depths_upsample=args.depths_up,
                          num_heads=args.heads_number, window_size=args.window_size).to(args.device)
+        # model = SwinLSTM(img_size= img_sizes, patch_size=patch_size,
+        #                  in_chans=1, embed_dim=embed_dim,
+        #                  depths_downsample=args.depths_down, depths_upsample=args.depths_up,
+        #                  num_heads=args.heads_number, window_size=args.window_size).to(args.device)
 
     optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
 
     criterion = nn.MSELoss()
 
-    train_dataset = Moving_MNIST(args, split='train')
-    valid_dataset = Moving_MNIST(args, split='valid')
+    # train_loader = DataLoader(train_dataset, batch_size=args.train_batch_size,
+    #                           num_workers=args.num_workers, shuffle=True, pin_memory=True, drop_last=True)
 
-    train_loader = DataLoader(train_dataset, batch_size=args.train_batch_size,
-                              num_workers=args.num_workers, shuffle=True, pin_memory=True, drop_last=True)
+    # valid_loader = DataLoader(valid_dataset, batch_size=args.valid_batch_size,
+    #                          num_workers=args.num_workers, shuffle=False, pin_memory=True, drop_last=True)
 
-    valid_loader = DataLoader(valid_dataset, batch_size=args.valid_batch_size,
-                             num_workers=args.num_workers, shuffle=False, pin_memory=True, drop_last=True)
-
-    return model, criterion, optimizer, train_loader, valid_loader
+    return model, criterion, optimizer, dataloader_train, dataloader_test
 
 def main():
     args = get_args()
@@ -65,7 +182,7 @@ def main():
             plot_loss(valid_losses, 'valid', epoch, args.res_dir, args.epoch_valid)
 
             if mse < best_metric[1]:
-                torch.save(model.state_dict(), f'{model_dir}/trained_model_state_dict')
+                torch.save(model.state_dict(), f'{model_dir}/trained_model_state_dict_mse_{mse}')
                 best_metric = (epoch, mse, ssim)
 
             logger.info(f'[Current Best] EP:{best_metric[0]:04d} MSE:{best_metric[1]:.4f} SSIM:{best_metric[2]:.4f}')
